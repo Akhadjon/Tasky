908f2f1d8b656466b5e40bd87302782e988dbba5
Akhadjon Abdukhalilov <abduxalilovahadjon97@gmail.com>
2020-09-09 22:25:55 +0500

    Initial Commit

---
+++ Tasky/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Tasky
+//
+//  Created by Akhadjon Abdukhalilov on 9/9/20.
+//  Copyright © 2020 Akhadjon Abdukhalilov. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
